4d34ba2432a8ee4d37da15f886706d6991bab760
AaronCurrie <93119982+AaronCurrie@users.noreply.github.com>
2022-02-10 13:35:21 +1100

    rotate the arrows on click

--- Resources/main.js
+++ Resources/main.js
@@ -10,6 +10,12 @@ const answer2 = document.getElementById('answer2');
 const answer3 = document.getElementById('answer3');
 const answer4 = document.getElementById('answer4');
 
+const arrow0 = document.getElementById('arrow0');
+const arrow1 = document.getElementById('arrow1');
+const arrow2 = document.getElementById('arrow2');
+const arrow3 = document.getElementById('arrow3');
+const arrow4 = document.getElementById('arrow4');
+
 let clicked0 = false;
 let clicked1 = false;
 let clicked2 = false;
@@ -40,9 +46,18 @@ function unclickall() {
     clicked4 = false;
 }
 
-function click(answer, question,) {
+function rotateBack() {
+    arrow0.style.transform = 'rotate(0deg)';
+    arrow1.style.transform = 'rotate(0deg)';
+    arrow2.style.transform = 'rotate(0deg)';
+    arrow3.style.transform = 'rotate(0deg)';
+    arrow4.style.transform = 'rotate(0deg)';
+}
+
+function click(answer, question, arrow) {
     answer.style.display = 'block';
-    question.style.fontWeight = "700";       
+    question.style.fontWeight = "700";
+    arrow.style.transform = 'rotate(180deg)';       
 }   
 
 function unclick(answer, question) {
@@ -50,29 +65,19 @@ function unclick(answer, question) {
     question.style.fontWeight = "400";
 }
 
-function go(answer, question, clicked) {
-    close();
-    noFont();
-    if(clicked === false) {
-        click(answer, question);
-        unclickall();
-        clicked = true;   
-    } else if(clicked === true) {
-        unclick(answer, question);
-        unclickall();
-    }
-}
 
 question0.addEventListener('click', function() {
     close();
     noFont();
     if(clicked0 === false) {
-        click(answer0, question0);
-        unclickall()
+        rotateBack();
+        click(answer0, question0, arrow0);
+        unclickall();
         clicked0 = true; 
     } else if(clicked0 === true) {
-        unclick(answer0, question0);
+        unclick(answer0, question0, arrow0);
         unclickall();
+        rotateBack();
     }
 } )
 
@@ -80,12 +85,14 @@ question1.addEventListener('click', function() {
     close();
     noFont();
     if(clicked1 === false) {
-        click(answer1, question1);
+        rotateBack();
+        click(answer1, question1, arrow1);
         unclickall();
         clicked1 = true;   
     } else if(clicked1 === true) {
         unclick(answer1, question1);
         unclickall();
+        rotateBack();
     }
 } )
 
@@ -93,12 +100,14 @@ question2.addEventListener('click', function() {
     close();
     noFont();
     if(clicked2 === false) {
-        click(answer2, question2);
+        rotateBack();
+        click(answer2, question2, arrow2);
         unclickall();
         clicked2 = true;   
     } else if(clicked2 === true) {
         unclick(answer2, question2);
         unclickall();
+        rotateBack();
     }
 } )
 
@@ -106,12 +115,14 @@ question3.addEventListener('click', function() {
     close();
     noFont();
     if(clicked3 === false) {
-        click(answer3, question3);
+        rotateBack();
+        click(answer3, question3, arrow3);
         unclickall();
         clicked3 = true;   
     } else if(clicked3 === true) {
         unclick(answer3, question3);
         unclickall();
+        rotateBack();
     }
 } )
 
@@ -119,12 +130,14 @@ question4.addEventListener('click', function() {
     close();
     noFont();
     if(clicked4 === false) {
-        click(answer4, question4);
+        rotateBack();
+        click(answer4, question4, arrow4);
         unclickall();
         clicked4 = true;   
     } else if(clicked4 === true) {
         unclick(answer4, question4);
         unclickall();
+        rotateBack();
     }
 } )
 
