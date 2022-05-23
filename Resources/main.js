f8a9781a64b7acaee1c7a1bc6979c457d0530de2
AaronCurrie <93119982+AaronCurrie@users.noreply.github.com>
2022-05-23 17:30:12 +0100

    Improved the JS

--- Resources/main.js
+++ Resources/main.js
@@ -1,3 +1,5 @@
+/*
+
 const question0 = document.getElementById('question0');
 const question1 = document.getElementById('question1');
 const question2 = document.getElementById('question2');
@@ -141,3 +143,41 @@ question4.addEventListener('click', function() {
     }
 } )
 
+*/
+
+const items = [...document.querySelectorAll('article')]
+
+function closeAll(array) {
+    array.forEach(item => {
+            item.querySelector('.answer').style.display = 'none'
+            item.querySelector('.question').children[0].style.fontWeight = "400";
+            item.querySelector('img').style.transform = 'rotate(0deg)';
+            item.classList.remove('active');
+        });
+}
+
+function close(array) {
+    array.forEach(item => {
+            item.querySelector('.answer').style.display = 'none'
+            item.querySelector('.question').children[0].style.fontWeight = "400";
+            item.querySelector('img').style.transform = 'rotate(0deg)';
+        });
+}
+
+items.forEach(item => {
+    item.addEventListener('click', (e) => {
+        closeAll(items);
+        const clickedItem = e.target.closest('article');
+        const itemAnswer = clickedItem.querySelector('.answer');
+        const itemQuestion = clickedItem.querySelector('.question').children[0];
+        const itemArrow = clickedItem.querySelector('img');
+        const clickedItemActive = clickedItem.classList.contains('active');
+        itemAnswer.style.display = 'block';
+        itemQuestion.style.fontWeight = "700";
+        itemArrow.style.transform = 'rotate(180deg)';
+        clickedItem.classList.add('active');
+        if(clickedItemActive) {
+            closeAll(items)
+        }
+    })
+})
